a2e55ebbe1e0368347f1ae2e98209efebb418f9f
Marco Aurelio Prado dos Santos Vidoca <Marco Aurelio Prado dos Santos Vidoca>
2016-10-14 19:05:16 -0300

    Backend on google cloud

--- app/src/paid/java/com/udacity/gradle/builditbigger/GetRandomJokeEndpointAsyncTask.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/GetRandomJokeEndpointAsyncTask.java
@@ -25,7 +25,7 @@ public class GetRandomJokeEndpointAsyncTask extends AsyncTask<Pair<OnAsyncTaskEn
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("https://builditbigger17.appspot.com/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
